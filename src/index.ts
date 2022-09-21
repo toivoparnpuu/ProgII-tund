b172e012a5f38e7fd1b35649bc3f6fc1035a566e
Toivo Parnpuu <tparnpuu@tlu.ee>
2022-09-21 23:01:27 +0300

    add user api - poolik

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,16 @@ import express, { Request, Response, Express } from 'express';
 
 const app: Express = express();
 const port: number = 3000;
+
+app.use(express.json);
+
+interface IUser {
+    firstName: string,
+    lastName: string,
+    email: string,
+    password: string
+}
+
 const users = [
     {
         firstName:"Juhan",
@@ -10,6 +20,7 @@ const users = [
         password: "juhan"
     }
 ]
+
 
 
 app.get('/api/v1/healt', (req: Request, res: Response) => {
@@ -23,6 +34,16 @@ app.get('/api/v1/users', (req: Request, res: Response) => {
     res.status(200).json({
       success: true,
       message: "list of users",
+      users,
+    });
+  });
+
+  app.post('/api/v1/users', (req: Request, res: Response) => {
+    const {firstName, lastName, email, password} =req.body;
+
+    res.status(201).json({
+      success: true,
+      message: "user created",
       users,
     });
   });
