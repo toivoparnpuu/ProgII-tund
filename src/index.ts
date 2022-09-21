bca66285f48e3375886c878c06b752fa83c60abb
Toivo Parnpuu <tparnpuu@tlu.ee>
2022-09-21 22:18:57 +0300

    healt, users api

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,30 @@ import express, { Request, Response, Express } from 'express';
 
 const app: Express = express();
 const port: number = 3000;
+const users = [
+    {
+        firstName:"Juhan",
+        lastName: "Juurikas",
+        email: "juhan@juurikas.ee",
+        password: "juhan"
+    }
+]
 
-app.get('/', (req: Request, res: Response) => {
+
+app.get('/api/v1/healt', (req: Request, res: Response) => {
   res.status(200).json({
     success: true,
     message: 'Hello world!',
   });
 });
+
+app.get('/api/v1/users', (req: Request, res: Response) => {
+    res.status(200).json({
+      success: true,
+      message: "list of users",
+      users,
+    });
+  });
 
 app.listen(port, () => {
   // eslint-disable-next-line no-console
